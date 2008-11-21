a0b794f0d97c1d8e1a8b3acc71d79ea3986944a9
FreedomCoder <matiasbrutti@gmail.com>
2008-11-21 14:27:46 -0200

    New clas Twitty that let's you twitt from irb and/or post your pastie with a simple >> twitt pastie

---
+++ lib/utility_belt/twitty.rb
@@ -0,0 +1,27 @@
+# automate creating twitts
+%w{net/http utility_belt}.each {|lib| require lib}
+UtilityBelt.equip(:clipboard)
+
+module UtilityBelt
+  module Twitty
+    def twitt(message = nill)
+      message ||= Clipboard.read if Clipboard.available?
+      if message.length > 160
+        Raise ArgumentError "Message should be shorter than 160 characters"
+      else
+        http = Net::HTTP.new("twitter.com",80)
+        http.start do |http|
+          request = Net::HTTP::Post.new("/statuses/update.json")
+          request.basic_auth(ENV['TWITTER_USER'], ENV['TWITTER_PWD'])
+          response = http.request(request, "status=#{URI.escape(message)}")
+          case response
+            when Net::HTTPSuccess, Net::HTTPRedirection
+                puts "Success! message: #{message} uploaded"
+            else
+                response.error!
+          end
+        end
+      end
+    end
+  end
+end
