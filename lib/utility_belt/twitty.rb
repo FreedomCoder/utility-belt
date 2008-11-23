bc3422fecb8732fbfbe646209bb8cc19f2d6835f
FreedomCoder <matiasbrutti@gmail.com>
2008-11-23 11:13:14 -0200

    More Changes to twitty and now I have a twitt command for the CLI. Sorry about the Rspec (I will do all of them ASAP.).

--- lib/utility_belt/twitty.rb
+++ lib/utility_belt/twitty.rb
@@ -7,7 +7,7 @@ module UtilityBelt
     def twitt(message = nill)
       message ||= Clipboard.read if Clipboard.available?
       if message.length > 160
-        Raise ArgumentError "Message should be shorter than 160 characters"
+        Raise ArgumentError, "Message should be shorter than 160 characters"
       else
         http = Net::HTTP.new("twitter.com",80)
         http.start do |http|
@@ -16,7 +16,7 @@ module UtilityBelt
           response = http.request(request, "status=#{URI.escape(message)}")
           case response
             when Net::HTTPSuccess, Net::HTTPRedirection
-                return "Success! message: #{message} uploaded"
+                return "Success!"
             else
                 return response.error!
           end
