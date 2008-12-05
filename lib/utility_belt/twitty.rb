08811429ccf9b67090bdcbbfa26d1e5e7123286f
FreedomCoder <matiasbrutti@gmail.com>
2008-12-05 16:03:09 -0200

    Adding fetch_twitt.

--- lib/utility_belt/twitty.rb
+++ lib/utility_belt/twitty.rb
@@ -1,5 +1,5 @@
 # automate creating twitts
-%w{net/http utility_belt}.each {|lib| require lib}
+%w{net/http json platform utility_belt}.each {|lib| require lib}
 UtilityBelt.equip(:clipboard)
 
 module UtilityBelt
@@ -20,6 +20,36 @@ module UtilityBelt
             else
                 return response.error!
           end
+        end
+      end
+    end
+    def parse_response(body)
+      minih = {}
+      JSON.parse(body).each do |twitt|
+          minih[twitt["user"]["name"]] = twitt["text"]
+      end
+      minih.each_pair do |key,value| 
+        case Platform::IMPL
+        when :macosx || :linux
+          puts "\033[31m#{key}\033\[0m => \033[33m#{value}\033\[0m"
+        when :mswin
+          puts "#{key} => #{value}"
+        end
+      end
+      true
+    end
+
+    def fetch_twitt
+      http = Net::HTTP.new("twitter.com",80)
+      http.start do |http|
+        request = Net::HTTP::Get.new("/statuses/friends_timeline.json")
+        request.basic_auth(ENV['TWITTER_USER'], ENV['TWITTER_PWD'])
+        response = http.request(request, "")
+        case response
+         when Net::HTTPSuccess, Net::HTTPRedirection
+             return parse_response(response.body)
+         else
+             return response.error!
         end
       end
     end
--- utility_belt.gemspec
+++ utility_belt.gemspec
@@ -1,7 +1,7 @@
 require 'rubygems' 
 SPEC = Gem::Specification.new do |s| 
   s.name = "utility_belt"
-  s.version = "1.0.8"
+  s.version = "1.1.1"
   s.author = "Giles Bowkett"
   s.email = "gilesb@gmail.com"
   s.homepage = "http://utilitybelt.rubyforge.org"
@@ -19,4 +19,5 @@ SPEC = Gem::Specification.new do |s|
   s.add_dependency("wirble", ">= 0.1.2")
   s.add_dependency("aws-s3", ">= 0.4.0")
   s.add_dependency("Platform", ">= 0.4.0")
+  s.add_dependency("json", ">= 1.1.3")
 end 
